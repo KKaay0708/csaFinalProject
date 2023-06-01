8c651458e4fe679f6f152c45ab50228d75487bf4
KKaay0708 <110819885+KKaay0708@users.noreply.github.com>
2023-05-31 19:51:42 -0700

    initial commit

---
+++ finalProject/barbarian.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class barbarian{
+    private int strength = 3;
+    private int agility = 1;
+    private int stamina = 4;
+    private int intelligence = 1;
+    private int spirit = 1;
+
+    public barbarian(int s, int a, int st, int i, int sp){
+        strength += s;
+        agility += a;
+        stamina += st;
+        intelligence += i;
+        spirit += sp;
+    }
+
+    private void addTalentPoints(int s, int a, int st, int i, int sp){
+        
+    }
+}
+
